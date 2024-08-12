1a249fe6b19c4a85f7f4f489f427b32e6230b646
Dhruvin558 <dhruvinmaniya0558@gmail.com>
2024-08-13 01:15:25 +0530

    page 105 row working

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -5,9 +5,15 @@ function Row() {
   return (
 <>
 <div>
-<p className='rowbg123'>
-    <img src='Image/Rectangle 28.png' alt='black bg'  />
-</p>
+<div className='rowbg123'>
+    {/* <img src='Image/Rectangle 28.png' alt='black bg'  /> */}.
+<l className='Row1'>500+</l>
+{/* <l className='Row2'>Happy Members<br/>
+Our community is growing fast</l>
+<l className='Row3'>5+</l>
+<l className="Row4">Year Experience <br/>
+Experience in various workouts</l> */}
+</div>
 </div>
 </>
   )
