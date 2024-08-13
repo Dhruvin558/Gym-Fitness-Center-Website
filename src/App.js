33cc6a267ff8933d3ae768968e42f2e4806cc78c
Dhruvin558 <dhruvinmaniya0558@gmail.com>
2024-08-13 12:20:30 +0530

    row Complete

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import './App.css';
 import Home from './Components/Home';
 import Navbar1 from './Components/Navbar1';
 import Row from './Components/Row';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
   <>
-  <Navbar1 /> 
-  <Home />
+  <Router>
+  <Navbar1/>
+  <Routes> 
+  {/* <Route path='/' element={<Navbar1/>} /> */}
+  <Route path='/' element={<Home />} />
+  {/* <Route path='/' element={<Row />} /> */}
+  </Routes>
   <Row />
   
+  </Router>
   
    </>
   );
--- src/Components/Row.js
+++ src/Components/Row.js
@@ -1,22 +1,35 @@
-import React from 'react'
-import './Row.css'
+import React from "react";
+import "./Row.css";
 
 function Row() {
   return (
-<>
-<div>
-<div className='rowbg123'>
-    {/* <img src='Image/Rectangle 28.png' alt='black bg'  /> */}.
-<l className='Row1'>500+</l>
-{/* <l className='Row2'>Happy Members<br/>
-Our community is growing fast</l>
-<l className='Row3'>5+</l>
-<l className="Row4">Year Experience <br/>
-Experience in various workouts</l> */}
-</div>
-</div>
-</>
-  )
+    <>
+      <div className="rowbg123">
+        {/* <img src='Image/Rectangle 28.png' alt='black bg'  /> */}.
+        <l className="Row1">500+</l>
+        <p className="Row2">
+          Happy Members
+          <br /> Our community is growing fast
+        </p>
+        <l className="Row3">5+</l>
+        <p className="Row4">
+          Year Experience <br />
+          Experience in various workouts
+        </p>
+        <l className="Row5">13+</l>
+        <p className="Row6">
+          Certified Trainers
+          <br />
+          Guidance at every step.
+        </p>
+        <l className="Row7">90%</l>
+        <p className="Row8">
+          Customer Satisfaction <br />
+          We ensure your progress satisfaction
+        </p>
+      </div>
+    </>
+  );
 }
 
-export default Row
+export default Row;
